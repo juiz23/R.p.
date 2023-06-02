5221783fa399bc55c08bec711c30f9b951a6719b
juiz23 <134434494+juiz23@users.noreply.github.com>
2023-06-02 19:36:34 +0000

    lgsf

---
+++ programa.js
@@ -0,0 +1,10 @@
+let placar = document.querySelector('placar');
+placar.textcontent ='pontos:0';
+
+let cartas = documents.querySelector('carta');
+
+let imgcarta = document.createElement('img');
+imgcarta.src="imagem/tokito.jpg";
+imgcarta.width="150";
+imgcarta.height="150";
+ let textocarta = document
